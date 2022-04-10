707ff27406d6b1a63320606e1586052a3860c406
Harvey Brezina Conniffe <harvey@Harveys-MacBook-Pro.local>
2022-04-10 16:15:09 +0100

    Created class to store path data.

---
+++ java/Path.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+
+public class Path {
+	ArrayList<Integer> stops;
+	double cost;
+
+	public Path(ArrayList<Integer> stops, double cost) {
+		this.stops = stops;
+		this.cost = cost;
+	}
+}
